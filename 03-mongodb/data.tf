3af96360e55d8d215129041f57e5e0d453c1cab8
Venkata Chowdari Udayagiri <145365456+VChowdary351@users.noreply.github.com>
2024-07-28 11:46:13 +0530

    Delete  directory

--- 03-mongodb/data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_ssm_parameter" "vpc_id" {
-    name = "/roboshop/dev/vpc_id"  #${var.project_name}${var.env}/vpc_id
-  
-}
--- 03-mongodb/main.tf
+++
@@ -1,9 +0,0 @@
-module "mongodb_sg" {
-    source = "../../Terraform-security-group"
-    project_name = var.project_name
-    sg_name = "MongoDB"
-    sg_description = var.sg_description
-    #ingress_rules = var.ingress_rules
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
-    common_tags = var.common_tags
-}
